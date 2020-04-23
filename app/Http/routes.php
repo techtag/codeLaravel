8310ae74222fc716c0787cd056c7caccbd2da027
Naresh <naresh1934@gmail.com>
2020-04-23 14:42:58 -0500

    Admin master page setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,3 +22,9 @@ Route::get('/home', 'HomeController@index');
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/admin',function(){
+	return view('admin.index');
+});
+
+Route::resource('admin/users','AdminUsersController');
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,2 @@
-index.blade.php
+@extends('layouts.admin')
+
